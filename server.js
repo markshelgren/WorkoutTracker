6f8491eeb0e690bf038c84719c198f8f4736a0d5
Mark Shelgren <markshelgren@outlook.com>
2021-01-23 15:42:50 -0800

    Changes to the routes - resolve exercise issue

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//   useNewUrlParser: true
-// });
+// Connection parameters between Heroku or localhost and MongoDB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -24,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useFindAndModify: false,
 });
 
+// Create a new workout
 app.post("/api/workouts", ({ body }, res) => {
 	db.Workout.create(body)
 		.then((data) => {
@@ -35,6 +34,7 @@ app.post("/api/workouts", ({ body }, res) => {
 		});
 });
 
+// Update and existing workout
 app.put("/api/workouts/:id", ({ body, params }, res) => {
 	db.Workout.findByIdAndUpdate(
 		params.id,
@@ -50,6 +50,7 @@ app.put("/api/workouts/:id", ({ body, params }, res) => {
 		});
 });
 
+// Retrieve all workouts
 app.get("/api/workouts", (req, res) => {
 	db.Workout.find({})
 		.then((data) => {
@@ -61,6 +62,7 @@ app.get("/api/workouts", (req, res) => {
 		});
 });
 
+// Retrieve the last seven days workouts
 app.get("/api/workouts/range", (req, res) => {
 	db.Workout.find({})
 		.limit(7)
@@ -72,16 +74,20 @@ app.get("/api/workouts/range", (req, res) => {
 			res.json(err);
 		});
 });
+
+// Display the exercise page
 app.get("/exercise", function (req, res) {
 	console.log("In /exercise html route");
 	res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
+// Display the stats page
 app.get("/stats", function (req, res) {
 	console.log("In /stats html route");
 	res.sendFile(path.join(__dirname, "public/stats.html"));
 });
 
+// Start the server and listen for some routes
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
 });
